69bd9db780d14f5fd80c0c9dfdc8cad9c6002f41
Kamok1 <kkamiss12@gmail.com>
2025-05-28 19:41:12 +0200

    Refactor machine learning notebooks; update data loading and preprocessing functions, and adjust train-validation-test split ratios

--- ai_models/shared.py
+++ ai_models/shared.py
@@ -10,7 +10,7 @@ def load_processed_data(path="../data/students.csv", sep=";", should_process=Tru
         df = process_data(df)
     return df
 
-def load_train_with_validation_data(X, y, test_size=0.15, val_size=0.15, random_state=1):
+def load_train_with_validation_data(X, y, test_size=0.20, val_size=0.10, random_state=1):
     X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=(test_size + val_size), random_state=random_state)
     X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size= 1 - (val_size / (test_size + val_size)), random_state=random_state)
     return X_train, X_val, X_test, y_train, y_val, y_test
